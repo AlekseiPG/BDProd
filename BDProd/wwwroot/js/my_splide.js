c8641d82a0f4e202e24ae074cc7568ebcaa2914f
Aleksei Pavlyshin-Gadais <al_pavlyshin-gadais@protonmail.com>
2024-04-05 15:24:40 +0200

    05/04

--- BDProd/wwwroot/js/my_splide.js
+++ BDProd/wwwroot/js/my_splide.js
@@ -111,24 +111,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
         var supprimerBtn = this;
 
-        var confirmBtn = document.createElement('div');
-        confirmBtn.classList.add('pseudo_btn_wrap_in');
-        confirmBtn.style.marginRight = '10px';
-        confirmBtn.innerHTML = '<div class="pseudo_btn my_red">Oui</div>';
+        var confirmBtn1 = document.createElement('div');
+        confirmBtn1.classList.add('pseudo_btn_wrap_in');
+        confirmBtn1.style.marginRight = '10px';
+        confirmBtn1.innerHTML = '<div class="pseudo_btn my_red">Oui</div>';
 
-        var cancelBtn = document.createElement('div');
-        cancelBtn.classList.add('pseudo_btn_wrap_in');
-        cancelBtn.innerHTML = '<div class="pseudo_btn">No</div>';
+        var cancelBtn1 = document.createElement('div');
+        cancelBtn1.classList.add('pseudo_btn_wrap_in');
+        cancelBtn1.innerHTML = '<div class="pseudo_btn">No</div>';
 
-        confirmBtn.style.display = 'inline-block';
-        cancelBtn.style.display = 'inline-block';
+        confirmBtn1.style.display = 'inline-block';
+        cancelBtn1.style.display = 'inline-block';
 
-        this.parentElement.insertBefore(confirmBtn, this);
-        this.parentElement.insertBefore(cancelBtn, this);
+        this.parentElement.insertBefore(confirmBtn1, this);
+        this.parentElement.insertBefore(cancelBtn1, this);
 
         this.style.display = 'none';
 
-        confirmBtn.addEventListener('click', function () {
+        confirmBtn1.addEventListener('click', function () {
             var currentIndex = main.index;
             carouselImagesArray.splice(currentIndex, 1);
             console.log(carouselImagesArray);
@@ -141,46 +141,104 @@ document.addEventListener('DOMContentLoaded', function () {
             initializeSplideCarousels();
 
             supprimerBtn.style.display = 'block';
-            cancelBtn.remove();
+            cancelBtn1.remove();
             this.remove();
         });
 
-        cancelBtn.addEventListener('click', function () {
-
+        cancelBtn1.addEventListener('click', function () {
             supprimerBtn.style.display = 'block';
-            confirmBtn.remove();
+            confirmBtn1.remove();
             this.remove();
         });
     });
 
-});
+    $('#moveToTrash').on('click', function () {
 
-$(function () {
-    var modal = $("#imageModal");
-    var modalImg = $("#modalImage");
-    var span = $(".close");
+        var trashBtn = this;
 
-    function displayModal(imageSrc) {
-        modalImg.attr("src", imageSrc);
-        modal.css("display", "block");
-        console.log("display");
-    }
+        var confirmBtn2 = document.createElement('div');
+        confirmBtn2.classList.add('pseudo_btn_wrap_in');
+        confirmBtn2.style.marginRight = '10px';
+        confirmBtn2.innerHTML = '<div class="pseudo_btn my_red">Oui</div>';
 
-    span.on("click", function () {
-        modal.css("display", "none");
+        var cancelBtn2 = document.createElement('div');
+        cancelBtn2.classList.add('pseudo_btn_wrap_in');
+        cancelBtn2.innerHTML = '<div class="pseudo_btn">No</div>';
+
+        confirmBtn2.style.display = 'inline-block';
+        cancelBtn2.style.display = 'inline-block';
+
+        this.parentElement.insertBefore(confirmBtn2, this);
+        this.parentElement.insertBefore(cancelBtn2, this);
+
+        this.style.display = 'none';
+
+        confirmBtn2.addEventListener('click', function () {
+
+            var currentIndex = main.index;
+            imageUrl = carouselImagesArray[currentIndex];
+            console.log(imageUrl);
+            if (imageUrl != 0) {
+                var folderPath = imageUrl.substring(0, imageUrl.lastIndexOf('/'));
+                $.ajax({
+                    url: '/ImageSelection/MoveToTrash',
+                    type: 'POST',
+                    data: { images: imageUrl, folderPath: folderPath },
+                    success: function (data) {
+                        carouselImagesArray.splice(currentIndex, 1);
+                        console.log(carouselImagesArray);
+                        var carouselImagesArraySetTri = JSON.stringify(carouselImagesArray);
+                        localStorage.setItem('carouselImagesArray', carouselImagesArraySetTri);
+                        populateCarousel(carouselImagesArray, carouselImages, carouselImagesThumb);
+                        initializeSplideCarousels();
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        console.error('Erreur lors du déplacement des images:', errorThrown);
+                    }
+                });
+            }
+
+            trashBtn.style.display = 'block';
+            cancelBtn2.remove();
+            this.remove();
+        });
+
+        cancelBtn2.addEventListener('click', function () {
+            trashBtn.style.display = 'block';
+            confirmBtn2.remove();
+            this.remove();
+        });
+
     });
 
-    $(window).on("click", function (event) {
-        if (event.target == modal[0]) {
-            modal.css("display", "none");
+    $(function () {
+        var modal = $("#imageModal");
+        var modalImg = $("#modalImage");
+        var span = $(".close");
+
+        function displayModal(imageSrc) {
+            modalImg.attr("src", imageSrc);
+            modal.css("display", "block");
+            console.log("display");
         }
-    });
 
-    $('#main-carousel').on('click', '.zoomIcon', function (event) {
-        console.log("zOOOM");
-        var imageUrl = $(this).closest('li').find('img').attr('src');
-        console.log(imageUrl);
-        displayModal(imageUrl);
-        event.stopPropagation();
+        span.on("click", function () {
+            modal.css("display", "none");
+        });
+
+        $(window).on("click", function (event) {
+            if (event.target == modal[0]) {
+                modal.css("display", "none");
+            }
+        });
+
+        $('#main-carousel').on('click', '.zoomIcon', function (event) {
+            console.log("zOOOM");
+            var imageUrl = $(this).closest('li').find('img').attr('src');
+            console.log(imageUrl);
+            displayModal(imageUrl);
+            event.stopPropagation();
+        });
     });
 });
+
